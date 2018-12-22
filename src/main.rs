8e2a80f38da64dbf33127750548ab16286679a34
bobrowadam <adam@bigpanda.io>
2018-12-22 20:14:30 +0200

    Pattern matching

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,48 @@
 extern crate chrono;
+extern crate uuid;
 
 use chrono::Local;
-use std::time::Instant;
+use std::time::{Instant, SystemTime};
+use uuid::Uuid;
 
 #[derive(Debug)] // This is for formating purposes
+struct TopicPartition(String, u32);
+#[derive(Debug)]
 struct Incident {
-    _id: String,
-    start_time: u64,
+    _id: Uuid,
+    start_time: SystemTime,
     active: bool,
+    status: IncidentStatus,
 }
 
-struct TopicPartition(String, u32);
-
 fn main() {
-    // Structs:
-    let incident_1 = build_incident(String::from("someId"), 156277777, true);
+    structures();
+    print_date();
+    area_calc();
+    enum_stuff();
+    match_enums();
+}
+// Structs:
+fn structures() {
+    let incident_1 = build_incident(
+        Uuid::new_v5(&Uuid::NAMESPACE_DNS, "foo".as_bytes()),
+        SystemTime::now(),
+        true,
+        IncidentStatus::Ok,
+    );
     let incident_2 = Incident {
-        _id: String::from("other_id"),
+        _id: Uuid::new_v5(&Uuid::NAMESPACE_DNS, "foo".as_bytes()),
         ..incident_1
     };
+
     let tp = TopicPartition(String::from("correlation.workday.old-notifications"), 0);
-    println!("Incident 1 id: {:?}", incident_1);
     print!("incident 2 id: {}\n", incident_2._id);
-    print!("incident 2 start_time: {}\n", incident_2.start_time);
+    // print!("incident 2 start_time: {}\n", incident_2.start_time);
     print!("incident 2 is_active: {}\n", incident_2.active);
     print!("topic: {}; partition: {}\n", tp.0, tp.1);
+}
 
-    // Dates and Time:
-    print_date();
-
-    // Area calc
+fn area_calc() {
     let rectanglge = (5, 5);
     println!("Area of 5 * 5 is {}", area(rectanglge));
     let rect1 = Rectangle {
@@ -47,16 +60,19 @@ fn main() {
         rect2.area(),
         rect1.area_greater_than(&rect2)
     );
-
-    // Enums:
-    enum_stuff();
 }
 
-fn build_incident(_id: String, start_time: u64, active: bool) -> Incident {
+fn build_incident(
+    _id: Uuid,
+    start_time: SystemTime,
+    active: bool,
+    status: IncidentStatus,
+) -> Incident {
     Incident {
         _id,
         start_time,
         active,
+        status,
     }
 }
 
@@ -84,25 +100,60 @@ impl Rectangle {
     }
 }
 
-fn enum_stuff() {
-    #[derive(Debug)]
-    enum IncidentStatus {
-        Ok,
-    }
-    #[derive(Debug)]
-    struct Incident {
-        _id: String,
-        start_time: Instant,
-        active: bool,
-        status: IncidentStatus,
-    }
+#[derive(Debug)]
+enum IncidentStatus {
+    Ok,
+    NotOk { message: String, status: String },
+}
 
-    let critical_incident = Incident {
-        _id: String::from("some-id"),
-        start_time: Instant::now(),
+fn enum_stuff() {
+    let ok_incident = Incident {
+        _id: Uuid::new_v5(&Uuid::NAMESPACE_DNS, "foo".as_bytes()),
+        start_time: SystemTime::now(),
         active: true,
         status: IncidentStatus::Ok,
     };
 
-    println!(" Incident: {:?}", critical_incident);
+    let not_ok_incident = Incident {
+        _id: Uuid::new_v5(&Uuid::NAMESPACE_DNS, "foo".as_bytes()),
+        start_time: SystemTime::now(),
+        active: true,
+        status: IncidentStatus::NotOk {
+            message: String::from("Bad gateway"),
+            status: String::from("Critical"),
+        },
+    };
+
+    println!(" Incident: {:?}", ok_incident);
+    println!(" Incident: {:?}", not_ok_incident);
+}
+
+fn match_enums() {
+    let incident_3 = Incident {
+        _id: Uuid::new_v5(&Uuid::NAMESPACE_DNS, "foo".as_bytes()),
+        start_time: SystemTime::now(),
+        active: true,
+        status: IncidentStatus::NotOk {
+            message: String::from("high disk space"),
+            status: String::from("Warning"),
+        },
+    };
+    match incident_3.status {
+        IncidentStatus::NotOk {
+            ref message,
+            ref status,
+        }
+            if "high disk space" == message =>
+        {
+            println!("Not Ok!\nmessage: {},\nstatus: {}", message, status)
+        }
+        IncidentStatus::NotOk {
+            ref message,
+            ref status,
+        } => println!(
+            "Its kind of ok... \nmessage: {},\nstatus: {}",
+            message, status
+        ),
+        IncidentStatus::Ok => println!("Result 2"),
+    };
 }
